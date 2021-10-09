a3cc04468ebeb37e5accef9a9b20b8e3d2f03bc3
Michał Sukowski <sukowskimichal@gmail.com>
2021-10-09 11:51:14 +0200

    fun with creating server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+/*const http = require("http");
 
 const server = http.createServer((req, resp) => {
   console.log("Ktoś wszedł na server");
@@ -8,4 +8,23 @@ const server = http.createServer((req, resp) => {
 
 server.listen(3000, () => {
   console.log("Nasłuchuję na porcie 3000");
+});*/
+
+const http = require("http");
+
+const server = http.createServer((req, resp) => {
+  if (req.url === "/") {
+    resp.write("<h1>Strona główna</h1>");
+    resp.end();
+  } else if (req.url === "/kontakt") {
+    resp.write("<h1>Kontakt</h1>");
+    resp.end();
+  } else {
+    resp.write("Nie znaleziono adresu");
+    resp.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Słucham na 3000");
 });
